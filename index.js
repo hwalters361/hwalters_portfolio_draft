e6ac0b4fdf1ab6e241909146346452d74c5ea05e
Hayley Walters <hayleywalters@Hayleys-MacBook-Pro.local>
2021-10-06 19:18:51 -0700

    more cleaning up. Got the header image to move with the cursor again. Made a note that this website isn't totally safe for mobile yet. I just tested it and it looks alright but I'm still wary

--- index.js
+++ index.js
@@ -6,12 +6,8 @@ document.addEventListener('mousemove', (event) => {
     var y = event.clientY; //goes from 0 to 720 on macbook pro
     var w = window.innerWidth;
     var h = window.innerHeight;
-
-    var bottom_of_window = $(window).scrollTop() + $(window).height();
-    var top_of_header = $(this).position().top;
-    
-    header.style.left = x**0.6-(w/10) + "px";
+    header.style.left = (x)**0.6 - w/10 + "px";
     console.log(header.style.left);
-    header.style.top = y**0.5-(h/10) + "px";
+    header.style.top = (y)**0.6 - h/10 + "px";
     console.log(header.style.top);
 });
